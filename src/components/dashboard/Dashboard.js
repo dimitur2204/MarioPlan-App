14942bacd9834d7003afc2e456658ba206bf0b3d
Dimitar Nizamov <61479393+dimitur2204@users.noreply.github.com>
2020-10-22 11:39:40 +0300

    Connected redux to the dashboard

--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -1,16 +1,18 @@
 import React, { Component } from 'react';
 import ProjectList from '../projects/ProjectList';
 import Notifications from './Notifications';
+import {connect} from 'react-redux';
 
 
 class Dashboard extends Component {
     state = {  }
     render() { 
+        const {projects} = this.props;
         return ( 
             <div className="dashboard container">
                 <div className="row">
                     <div className="col s12 m6">
-                        <ProjectList></ProjectList>
+                        <ProjectList projects={projects}></ProjectList>
                     </div>
                     <div className="col s12 m5 offset-m1">
                         <Notifications></Notifications>
@@ -20,5 +22,11 @@ class Dashboard extends Component {
          );
     }
 }
+
+const mapStateToProps = (state) => {
+    return {
+        projects: state.project.projects
+    }
+}
  
-export default Dashboard;
+export default connect(mapStateToProps)(Dashboard);
--- src/store/reducers/projectReducer.js
+++ src/store/reducers/projectReducer.js
@@ -1,6 +1,12 @@
-const initState = {};
+const initState = {
+    projects:[
+        {id:'1', title:'first project', content:'Lorem ipsum dolor sit amet natam ne go znam some content here actually'},
+        {id:'2', title:'help me find peach', content:'Help Mario find peach he is so desperate and needs help'},
+        {id:'3', title:'egg hunt something', content:'Davaite da hodim za qica po burzo super mario nai qkata igra bratle'},
+    ]
+};
 
-const projectReducer = (state = initState,action) => {
+const projectReducer = (state = initState, action) => {
     return state;
 }
 
