93dcfd3c41c3447f8a6a63631b48c8cc08ddd9ee
Dimitar Nizamov <61479393+dimitur2204@users.noreply.github.com>
2020-10-22 12:05:40 +0300

    Added configuration for Firebase and Firestor

---
+++ src/config/fbConfig.js
@@ -0,0 +1,21 @@
+// Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  import firebase from 'firebase/app';
+  import 'firebase/firestore';
+  import 'firebase/auth';
+  var firebaseConfig = {
+    apiKey: "AIzaSyBaJSkM3-a67fa_ukUhUzLIoCoKiflhI6E",
+    authDomain: "marioplan-b682f.firebaseapp.com",
+    databaseURL: "https://marioplan-b682f.firebaseio.com",
+    projectId: "marioplan-b682f",
+    storageBucket: "marioplan-b682f.appspot.com",
+    messagingSenderId: "407061318286",
+    appId: "1:407061318286:web:c7209282754121b36d57df",
+    measurementId: "G-3R1W74V0QH"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.firestore().settings({ timestampsInSnapshots:true});
+  firebase.analytics();
+
+  export default firebase;
