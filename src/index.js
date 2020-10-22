a7fc52107b6a9fedf59c7e17978a293a55489757
Dimitar Nizamov <61479393+dimitur2204@users.noreply.github.com>
2020-10-22 14:25:04 +0300

    Changed the connection with react-redux-firebase from store enhancer to a provider component because of breaking changes

--- src/index.js
+++ src/index.js
@@ -7,21 +7,29 @@ import {createStore, applyMiddleware, compose} from 'redux';
 import rootReducer from './store/reducers/rootReducer';
 import {Provider} from 'react-redux';
 import thunk from 'redux-thunk';
-import {reduxFirestore, getFirestore} from 'redux-firestore';
-import {reactReduxFirebase, getFirebase} from 'react-redux-firebase';
+import {createFirestoreInstance, reduxFirestore, getFirestore} from 'redux-firestore';
+import {ReactReduxFirebaseProvider, getFirebase} from 'react-redux-firebase';
 import fbConfig from './config/fbConfig';
 
 const store = createStore(rootReducer, 
   compose(
   applyMiddleware(thunk.withExtraArgument({getFirebase,getFirestore})),
-  reduxFirestore(fbConfig)
   ));
+
+  const rrfProps = {
+    firebase:fbConfig,
+    config:{},
+    dispatch:store.dispatch,
+    createFirestoreInstance
+  }
 
 ReactDOM.render(
   <Provider store={store}>
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
+    <ReactReduxFirebaseProvider {...rrfProps}>
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
+    </ReactReduxFirebaseProvider>
   </Provider>,
     document.getElementById('root')
   
